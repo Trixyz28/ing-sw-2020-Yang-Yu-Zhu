60e1bfc08a7eb23efad0b086b015ade6ec480081
Zheng Maria Yu <10596129@polimi.it>
2020-03-30 11:24:19 +0200

    MVC Package

---
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model;
+
+public class Player {
+    private String playerID;
+    private String playerNickname;
+    private boolean isChallenger;
+    private String startPlayerID;
+}
